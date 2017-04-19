603091f6a9209c9aa1cd5d017360b25e5aa9bff9
Daniel Kapusta <daniel.kapusta@teamaol.com>
2017-04-18 23:21:58 -0400

    cleaned up to pass xo 0.18 tests

--- tasks/babel.js
+++ tasks/babel.js
@@ -1,27 +1,27 @@
 'use strict';
-var fs = require('fs');
-var path = require('path');
-var babel = require('babel-core');
+const fs = require('fs');
+const path = require('path');
+const babel = require('babel-core');
 
 module.exports = function (grunt) {
 	grunt.registerMultiTask('babel', 'Use next generation JavaScript, today', function () {
-		var options = this.options();
+		const options = this.options();
 
-		this.files.forEach(function (el) {
+		this.files.forEach(el => {
 			delete options.filename;
 			delete options.filenameRelative;
 
 			console.log('Converting', el.src.length, 'file(s)');
 
-			var code = el.src.map(function (filePath) {
-				var path = (process.platform === 'win32') ? filePath.replace(/\\/g, '/') : filePath;
+			const code = el.src.map(filePath => {
+				const path = (process.platform === 'win32') ? filePath.replace(/\\/g, '/') : filePath;
 				return fs.readFileSync(path, 'utf8');
 			});
 
 			options.sourceMapTarget = path.basename(el.dest);
 
-			var transformed = babel.transform(code.join('\n'), options);
-			var sourceMappingURL = '';
+			const transformed = babel.transform(code.join('\n'), options);
+			let sourceMappingURL = '';
 
 			if (transformed.map) {
 				sourceMappingURL = '\n//# sourceMappingURL=' + path.basename(el.dest) + '.map';
